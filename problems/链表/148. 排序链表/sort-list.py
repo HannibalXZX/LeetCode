e395375d4d5a077930890cc0eaf83408f36f566f
mac_2014 <624336638@qq.com>
2020-07-21 23:40:31 +0800

    :tada: 增加新题

---
+++ problems/链表/148. 排序链表/sort-list.py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*-
+#@Time  :    2020/7/21 6:13 下午
+#@Author:    Shaw
+#@mail  :    shaw@bupt.edu.cn
+#@File  :    sort-list.py
+#@Description：https://leetcode-cn.com/problems/sort-list/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+
+    ## 递归
+    def sortList_1(self, head: ListNode) -> ListNode:
+        # termination.
+        if not head or not head.next:
+            return head
+
+        # cut the LinkedList at the mid index.
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None  # save and cut.
+        # recursive for cutting.
+        left, right = self.sortList(head), self.sortList(mid)
+        # merge `left` and `right` linked list and return it.
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val:
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
+
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        slow = head
+        fast = head
+        # 用快慢指针分成两部分
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        # 找到左右部分, 把左部分最后置空
+        mid = slow.next
+        slow.next = None
+        # 递归下去
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        # 合并
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        dummy = ListNode(0)
+        p = dummy
+        l = left
+        r = right
+
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+                p = p.next
+            else:
+                p.next = r
+                r = r.next
+                p = p.next
+        if l:
+            p.next = l
+        if r:
+            p.next = r
+        return dummy.next
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
