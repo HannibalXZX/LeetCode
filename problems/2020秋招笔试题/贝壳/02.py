8445c5d5c1c6fb78423ef8250b4030d3384c25a9
mac_2014 <624336638@qq.com>
2020-09-12 12:51:29 +0800

    :fix: add notes

--- problems/2020秋招笔试题/贝壳/02.py
+++ problems/2020秋招笔试题/贝壳/02.py
@@ -2,6 +2,9 @@
 # 本题为考试单行多行输入输出规范示例，无需提交，不计分。
 import sys
 
+# 使用若干颜色给一排座位涂色，相邻位置之间不能同色
+# 测试用例过0%
+
 s = input()
 for line in sys.stdin:
     each = line.strip().split(" ")
@@ -17,4 +20,6 @@ for line in sys.stdin:
     dp[1] = m*(m-1)
     for i in range(1,n):
         dp[i] = dp[i-1] * (m-1)
+
+    # 不能结果再mod，每一步都要mod一下
     print(dp[n-1]%(10^9+7))
